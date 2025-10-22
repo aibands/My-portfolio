3f13a5a9cb96ab6ec61a74f85d7a0efa33a57720
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-10-22 13:58:52 -0700

    Compleated fintrack pro & about page

--- animate.js
+++ animate.js
@@ -49,3 +49,27 @@ navLinks.forEach((link) => {
   });
 });
 
+
+// about page - gallery
+  const images = document.querySelectorAll('.carousel-images img');
+  const nextBtn = document.querySelector('.next');
+  const prevBtn = document.querySelector('.prev');
+  let currentIndex = 0;
+
+  function showImage(index) {
+    const total = images.length;
+    if (index < 0) index = total - 1;
+    if (index >= total) index = 0;
+    currentIndex = index;
+    document.querySelector('.carousel-images').style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  nextBtn.addEventListener('click', () => {
+    showImage(currentIndex + 1);
+  });
+
+  prevBtn.addEventListener('click', () => {
+    showImage(currentIndex - 1);
+  });
+
+
